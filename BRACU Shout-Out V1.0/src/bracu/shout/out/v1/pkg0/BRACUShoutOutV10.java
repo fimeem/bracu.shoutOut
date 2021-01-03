6d880adff47cb3d73bfa9896282630e732f8dbe6
Fatema Islam Meem <fatema.islam.meem@g.bracu.ac.bd>
2021-01-03 14:57:44 +0600

    Bracu Shout Out
    Our project “Bracu Shout-Out” has been created to make university life better in a orderly way for both bracu students and faculties. This is an well-controlled database for keeping the track of classes, sections and time schedules. Students and faculties can communicate with each other through this platform for any announcement or queries regarding the courses.

---
+++ BRACU Shout-Out V1.0/src/bracu/shout/out/v1/pkg0/BRACUShoutOutV10.java
@@ -0,0 +1,15 @@
+package bracu.shout.out.v1.pkg0;
+
+/**
+ *
+ * @author Niloy
+ */
+public class BRACUShoutOutV10 
+{
+    public static void main(String[] args) 
+    {
+        SignIn a =new SignIn();
+        a.setVisible(true);
+    }
+    
+}
